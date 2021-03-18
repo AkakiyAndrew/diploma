9dcdbf7c4d7d5fa31731e55cb2cd5414ed3f019a
AkakiyAndrew <akakiy@email.ua>
2021-03-18 11:25:41 +0200

    18.03.2021

--- raylib_test/raylib_test/lab_3.cpp
+++ raylib_test/raylib_test/lab_3.cpp
@@ -19,21 +19,53 @@ int main(void)
     const int screenWidth = 1600;
     const int screenHeight = 900;
 
-    InitWindow(screenWidth, screenHeight, "image levels visualiser");
+    InitWindow(screenWidth, screenHeight, "image operators visualiser");
     SetTargetFPS(60);
     //ToggleFullscreen();
 
-    Image mainImage = LoadImage("input_photo.png");
+    Image mainImage = LoadImage("input_engine.png");
+    bool denoising = true;
+
     Image grayImage = ImageCopy(mainImage);
     ImageColorGrayscale(&grayImage);
 
     int arrSize = mainImage.width * mainImage.height;
-    Image grayRoberts = ImageCopy(grayImage);
+    int values[3][3] = {}, G, G1, G2, maskSum = 0;
+    int index;
 
     // Processing images
-    
-    int values[3][3] = {}, G, G1, G2;
-    int index;
+
+    int antiNoiseMask[3][3] = { { 1, 2, 1},
+                                { 2, 4, 2},
+                                { 1, 2, 1} };
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++) maskSum += antiNoiseMask[i][j];
+
+    if (denoising)
+    {
+        for (int y = 0; y < grayImage.height; y++)
+        {
+            for (int x = 0; x < grayImage.width; x++)
+            {
+                index = grayImage.width * y + x;
+                G = 0;
+
+                for (int i = -1; i < 2; i++)
+                    for (int j = -1; j < 2; j++)
+                        G += antiNoiseMask[i + 1][j + 1] * getBrightness(&grayImage, x + i, y + j);
+
+                G /= maskSum;
+
+                if (G < 0) G = 0;
+                if (G > 255) G = 255;
+
+                ((unsigned char*)grayImage.data)[index] = G;
+            }
+        }
+    }
+
+    Image grayRoberts = ImageCopy(grayImage);
 
     for (int y = 0; y < grayImage.height; y++)
     {
@@ -161,12 +193,75 @@ int main(void)
         }
     }
 
+    //Image grayPreviteTwo = ImageCopy(grayImage);
+
+    //for (int y = 0; y < grayPreviteTwo.height; y++)
+    //{
+    //    for (int x = 0; x < grayPreviteTwo.width; x++)
+    //    {
+    //        index = grayPreviteTwo.width * y + x;
+    //        G1 = 0;
+    //        G2 = 0;
+
+    //        for (int i = -1; i < 2; i++)
+    //            for (int j = -1; j < 2; j++)
+    //                G1 += operatorKirshX[i + 1][j + 1] * getBrightness(&grayPreviteTwo, x + i, y + j);
+
+    //        for (int i = -1; i < 2; i++)
+    //            for (int j = -1; j < 2; j++)
+    //                G2 += operatorKirshY[i + 1][j + 1] * getBrightness(&grayPreviteTwo, x + i, y + j);
+
+    //        G = sqrt(pow(G1, 2) + pow(G2, 2));
+
+    //        if (G < 0) G = 0;
+    //        if (G > 255) G = 255;
+
+    //        ((unsigned char*)grayPreviteTwo.data)[index] = G;
+    //    }
+    //}
+
+    //Image grayRobertsTwo = ImageCopy(grayImage);
+
+    //int operatorRobertsX[2][2] = { { -1, 0},
+    //                              {0, 1},};
+
+    //int operatorRobertsY[2][2] = { { 0, -1},
+    //                              {1, 0}, };
+
+    //for (int y = 0; y < grayRobertsTwo.height; y++)
+    //{
+    //    for (int x = 0; x < grayRobertsTwo.width; x++)
+    //    {
+    //        index = grayRobertsTwo.width * y + x;
+    //        G1 = 0;
+    //        G2 = 0;
+
+    //        for (int i = 0; i < 2; i++)
+    //            for (int j = 0; j < 2; j++)
+    //                G1 += operatorRobertsX[i][j] * getBrightness(&grayRobertsTwo, x + i, y + j);
+
+    //        for (int i = 0; i < 2; i++)
+    //            for (int j = 0; j < 2; j++)
+    //                G2 += operatorRobertsY[i][j] * getBrightness(&grayRobertsTwo, x + i, y + j);
+
+    //        G = sqrt(pow(G1, 2) + pow(G2, 2));
+
+    //        if (G < 0) G = 0;
+    //        if (G > 255) G = 255;
+
+    //        ((unsigned char*)grayRobertsTwo.data)[index] = G;
+    //    }
+    //}
+
     Texture mainTexture = LoadTextureFromImage(mainImage);
+    //Texture grayTexture = LoadTextureFromImage(grayImage);
     Texture grayTexture = LoadTextureFromImage(grayImage);
     Texture grayRobertsTexture = LoadTextureFromImage(grayRoberts);
     Texture grayPreviteTexture = LoadTextureFromImage(grayPrevite);
     Texture graySobelTexture = LoadTextureFromImage(graySobel);
     Texture grayKirshTexture = LoadTextureFromImage(grayKirsh);
+    //Texture grayRobertsTwoTexture = LoadTextureFromImage(grayRobertsTwo);
+    //Texture grayPreviteTwoTexture = LoadTextureFromImage(grayPreviteTwo);
 
     Camera2D camera = { 0 };
     camera.target = { screenWidth / 2, screenHeight / 2 };
@@ -174,7 +269,7 @@ int main(void)
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
     Vector2 cursor = GetMousePosition();
-    Vector2 lowerCorner = { 20, mainTexture.height*3 + 20 };
+    Vector2 lowerCorner = { 20, mainTexture.height*4 + 20 };
 
 
     // Main loop
@@ -183,10 +278,10 @@ int main(void)
         // Cursor moving
         cursor = GetMousePosition();
 
-        if (cursor.x < 50 && camera.target.x > 100) camera.target.x -= 10;
-        if (cursor.x > screenWidth - 50 && camera.target.x < mainTexture.width + grayTexture.width) camera.target.x += 10;
-        if (cursor.y < 50 && camera.target.y > 100) camera.target.y -= 10;
-        if (cursor.y > screenHeight - 50 && camera.target.y < lowerCorner.y) camera.target.y += 10;
+        if ((cursor.x < 50 && camera.target.x > 100) || IsKeyDown(KEY_A)) camera.target.x -= 10;
+        if ((cursor.x > screenWidth - 50 && camera.target.x < mainTexture.width + grayTexture.width) || IsKeyDown(KEY_D)) camera.target.x += 10;
+        if ((cursor.y < 50 && camera.target.y > 100 ) || IsKeyDown(KEY_W)) camera.target.y -= 10;
+        if ((cursor.y > screenHeight - 50 && camera.target.y < lowerCorner.y) || IsKeyDown(KEY_S)) camera.target.y += 10;
 
         // Camera zoom controls
         camera.zoom += ((float)GetMouseWheelMove() * 0.05f);
@@ -208,6 +303,8 @@ int main(void)
         DrawTexture(grayPreviteTexture, mainTexture.width + 50, mainTexture.height + 20, WHITE);
         DrawTexture(graySobelTexture, 20, mainTexture.height * 2 + 20, WHITE);
         DrawTexture(grayKirshTexture, mainTexture.width + 50, mainTexture.height * 2 + 20, WHITE);
+        //DrawTexture(grayRobertsTwoTexture, 20, mainTexture.height * 3 + 20, WHITE);
+        //DrawTexture(grayPreviteTwoTexture, mainTexture.width + 50, mainTexture.height*3 + 20, WHITE);
 
         EndDrawing();
     }
