cc2725786c9e37d596dc930f5d88ebc61825c716
AkakiyAndrew <akakiy@email.ua>
2021-04-09 18:43:44 +0300

    09.04.2021

--- game/Game/main.cpp
+++ game/Game/main.cpp
@@ -1,5 +1,12 @@
-//#include "raylib.h"
-//
+#include "PerlinNoise.h"
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include "raygui.h"
+#include "raylib.h"
+
 //int main(void)
 //{
 //    // Initialization
@@ -11,16 +18,16 @@
 //    const int cornerY = 200;
 //
 //    const int pixelsPerCell = 32;
-//    const int numOfRows = 10;
-//    const int numOfColumns = 10;
+//    const int height = 10;
+//    const int width = 10;
 //
 //    int cellByX, cellByY;
 //
-//    bool obstacles[numOfRows][numOfColumns] = {};
+//    bool obstacles[height][width] = {};
 //
 //    bool buildMode = false;
 //    Vector2 corner = { cornerX, cornerY };
-//    Vector2 rectangleSize = { numOfRows * pixelsPerCell, numOfColumns * pixelsPerCell };
+//    Vector2 rectangleSize = { height * pixelsPerCell, width * pixelsPerCell };
 //
 //    InitWindow(screenWidth, screenHeight, "raylib [core] simple map");
 //
@@ -48,20 +55,20 @@
 //        // Background
 //        ClearBackground(DARKGRAY);
 //        DrawRectangleV(corner, rectangleSize, RAYWHITE);
-//        for (int i = 1; i < numOfRows; i++)
+//        for (int i = 1; i < height; i++)
 //        {
 //            DrawLine(cornerX, cornerY + 32 * i, cornerX + rectangleSize.x, cornerY + 32 * i, GREEN);
 //        }
 //
-//        for (int j = 1; j < numOfColumns; j++)
+//        for (int j = 1; j < width; j++)
 //        {
 //            DrawLine(cornerX+pixelsPerCell*j, cornerY, cornerX + pixelsPerCell * j, cornerY+rectangleSize.y, GREEN);
 //        }
 //
 //        //drawing non-empty cells
-//        for (int i = 0; i < numOfRows; i++)
+//        for (int i = 0; i < height; i++)
 //        {
-//            for (int j = 0; j < numOfColumns; j++)
+//            for (int j = 0; j < width; j++)
 //            {
 //                if (obstacles[i][j])
 //                {
@@ -121,207 +128,282 @@
 //
 //    return 0;
 //}
-# include "PerlinNoise.h"
-# include <cassert>
-# include <iostream>
-# include <fstream>
-# include <sstream>
-#include <algorithm>
 
-
-# pragma pack (push, 1)
-struct BMPHeader
+int main(void)
 {
-	std::uint16_t bfType;
-	std::uint32_t bfSize;
-	std::uint16_t bfReserved1;
-	std::uint16_t bfReserved2;
-	std::uint32_t bfOffBits;
-	std::uint32_t biSize;
-	std::int32_t  biWidth;
-	std::int32_t  biHeight;
-	std::uint16_t biPlanes;
-	std::uint16_t biBitCount;
-	std::uint32_t biCompression;
-	std::uint32_t biSizeImage;
-	std::int32_t  biXPelsPerMeter;
-	std::int32_t  biYPelsPerMeter;
-	std::uint32_t biClrUsed;
-	std::uint32_t biClrImportant;
-};
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-static_assert(sizeof(BMPHeader) == 54);
-# pragma pack (pop)
+    InitWindow(screenWidth, screenHeight, "raylib [core] map generation");
 
-struct RGB
-{
-	double r = 0.0;
-	double g = 0.0;
-	double b = 0.0;
-	constexpr RGB() = default;
-	explicit constexpr RGB(double _rgb) noexcept
-		: r(_rgb), g(_rgb), b(_rgb) {}
-	constexpr RGB(double _r, double _g, double _b) noexcept
-		: r(_r), g(_g), b(_b) {}
-};
+    SetTargetFPS(60);
 
-class Image
-{
-private:
+    const int cornerX = 200;
+    const int cornerY = 200;
 
-	std::vector<RGB> m_data;
+    const int pixelsPerCell = 32;
+    const int height = 512;
+    const int width = 512;
 
-	std::int32_t m_width = 0, m_height = 0;
+    int cellByX, cellByY;
 
-	bool inBounds(std::int32_t y, std::int32_t x) const noexcept
-	{
-		return (0 <= y) && (y < m_height) && (0 <= x) && (x < m_width);
-	}
+    bool obstacles[height][width] = {};
 
-	static constexpr std::uint8_t ToUint8(double x) noexcept
-	{
-		return x >= 1.0 ? 255 : x <= 0.0 ? 0 : static_cast<std::uint8_t>(x * 255.0 + 0.5);
-	}
+    bool buildMode = false;
+  /*  Vector2 corner = { cornerX, cornerY };
+    Vector2 rectangleSize = { height * pixelsPerCell, width * pixelsPerCell };*/
 
-public:
-	
-	Image() = default;
+    unsigned char* pixels = new unsigned char[height * width];
 
-	Image(std::size_t width, std::size_t height)
-		: m_data(width* height)
-		, m_width(static_cast<std::int32_t>(width))
-		, m_height(static_cast<std::int32_t>(height)) {}
+    double frequency = 8;
+    int octaves = 8;
 
-	void set(std::int32_t x, std::int32_t y, const RGB& color)
-	{
-		if (!inBounds(y, x))
-		{
-			return;
-		}
+    const siv::PerlinNoise perlin(12345);
+    const double fx = width / frequency;
+    const double fy = height / frequency;
+    
+    int index = 0;
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            index = width * y + x;
+            pixels[index] = static_cast<unsigned char>(perlin.accumulatedOctaveNoise2D_0_1(x / fx, y / fy, octaves) *255.);
+        }
+    }
 
-		m_data[static_cast<std::size_t>(y) * m_width + x] = color;
-	}
+    Image preview = {
+        pixels,
+        width,
+        height,
+        UNCOMPRESSED_GRAYSCALE,
+        1
+    };
+    //UNCOMPRESSED_R8G8B8
+    Texture2D texture = LoadTextureFromImage(preview);
 
-	std::int32_t width() const
-	{
-		return m_width;
-	}
 
-	std::int32_t height() const
-	{
-		return m_height;
-	}
+    //--------------------------------------------------------------------------------------
 
-	bool saveBMP(const std::string& path)
-	{
-		const std::int32_t  rowSize = m_width * 3 + m_width % 4;
-		const std::uint32_t bmpsize = rowSize * m_height;
-		const BMPHeader header =
-		{
-			0x4d42,
-			static_cast<std::uint32_t>(bmpsize + sizeof(BMPHeader)),
-			0,
-			0,
-			sizeof(BMPHeader),
-			40,
-			m_width,
-			m_height,
-			1,
-			24,
-			0,
-			bmpsize,
-			0,
-			0,
-			0,
-			0
-		};
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {
+        BeginDrawing();
 
-		if (std::ofstream ofs{ path, std::ios_base::binary })
-		{
-			ofs.write(reinterpret_cast<const char*>(&header), sizeof(header));
+        // Background
+        ClearBackground(DARKGRAY);
+        
+        DrawTexture(texture, 0, 0, WHITE);
 
-			std::vector<std::uint8_t> line(rowSize);
+        EndDrawing();
+    }
 
-			for (std::int32_t y = m_height - 1; -1 < y; --y)
-			{
-				size_t pos = 0;
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
-				for (std::int32_t x = 0; x < m_width; ++x)
-				{
-					const RGB& col = m_data[static_cast<std::size_t>(y) * m_width + x];
-					line[pos++] = ToUint8(col.b);
-					line[pos++] = ToUint8(col.g);
-					line[pos++] = ToUint8(col.r);
-				}
-
-				ofs.write(reinterpret_cast<const char*>(line.data()), line.size());
-			}
-
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-};
-
-int main()
-{
-
-	Image image(512, 512);
-
-	std::cout << "---------------------------------\n";
-	std::cout << "* frequency [0.1 .. 8.0 .. 64.0] \n";
-	std::cout << "* octaves   [1 .. 8 .. 16]       \n";
-	std::cout << "* seed      [0 .. 2^32-1]        \n";
-	std::cout << "---------------------------------\n";
-
-	for (;;)
-	{
-		double frequency;
-		std::cout << "double frequency = ";
-		std::cin >> frequency;
-		frequency = std::clamp(frequency, 0.1, 64.0);
-
-		std::int32_t octaves;
-		std::cout << "int32 octaves    = ";
-		std::cin >> octaves;
-		octaves = std::clamp(octaves, 1, 16);
-
-		std::uint32_t seed;
-		std::cout << "uint32 seed      = ";
-		std::cin >> seed;
-
-		const siv::PerlinNoise perlin(seed);
-		const double fx = image.width() / frequency;
-		const double fy = image.height() / frequency;
-
-		for (std::int32_t y = 0; y < image.height(); ++y)
-		{
-			for (std::int32_t x = 0; x < image.width(); ++x)
-			{
-				const RGB color(perlin.accumulatedOctaveNoise2D_0_1(x / fx, y / fy, octaves));
-				image.set(x, y, color);
-			}
-		}
-
-		std::stringstream ss;
-		ss << 'f' << frequency << 'o' << octaves << '_' << seed << ".bmp";
-
-		if (image.saveBMP(ss.str()))
-		{
-			std::cout << "...saved \"" << ss.str() << "\"\n";
-		}
-		else
-		{
-			std::cout << "...failed\n";
-		}
-
-		char c;
-		std::cout << "continue? [y/n] >";
-		std::cin >> c;
-		if (c != 'y') break;
-		std::cout << '\n';
-	}
+    return 0;
 }
+
+
+//
+//# pragma pack (push, 1)
+//struct BMPHeader
+//{
+//	std::uint16_t bfType;
+//	std::uint32_t bfSize;
+//	std::uint16_t bfReserved1;
+//	std::uint16_t bfReserved2;
+//	std::uint32_t bfOffBits;
+//	std::uint32_t biSize;
+//	std::int32_t  biWidth;
+//	std::int32_t  biHeight;
+//	std::uint16_t biPlanes;
+//	std::uint16_t biBitCount;
+//	std::uint32_t biCompression;
+//	std::uint32_t biSizeImage;
+//	std::int32_t  biXPelsPerMeter;
+//	std::int32_t  biYPelsPerMeter;
+//	std::uint32_t biClrUsed;
+//	std::uint32_t biClrImportant;
+//};
+//
+//static_assert(sizeof(BMPHeader) == 54);
+//# pragma pack (pop)
+//
+//struct RGB
+//{
+//	double r = 0.0;
+//	double g = 0.0;
+//	double b = 0.0;
+//	constexpr RGB() = default;
+//	explicit constexpr RGB(double _rgb) noexcept
+//		: r(_rgb), g(_rgb), b(_rgb) {}
+//	constexpr RGB(double _r, double _g, double _b) noexcept
+//		: r(_r), g(_g), b(_b) {}
+//};
+//
+//class Image
+//{
+//private:
+//
+//	std::vector<RGB> m_data;
+//
+//	std::int32_t m_width = 0, m_height = 0;
+//
+//	bool inBounds(std::int32_t y, std::int32_t x) const noexcept
+//	{
+//		return (0 <= y) && (y < m_height) && (0 <= x) && (x < m_width);
+//	}
+//
+//	static constexpr std::uint8_t ToUint8(double x) noexcept
+//	{
+//		return x >= 1.0 ? 255 : x <= 0.0 ? 0 : static_cast<std::uint8_t>(x * 255.0 + 0.5);
+//	}
+//
+//public:
+//	
+//	Image() = default;
+//
+//	Image(std::size_t width, std::size_t height)
+//		: m_data(width* height)
+//		, m_width(static_cast<std::int32_t>(width))
+//		, m_height(static_cast<std::int32_t>(height)) {}
+//
+//	void set(std::int32_t x, std::int32_t y, const RGB& color)
+//	{
+//		if (!inBounds(y, x))
+//		{
+//			return;
+//		}
+//
+//		m_data[static_cast<std::size_t>(y) * m_width + x] = color;
+//	}
+//
+//	std::int32_t width() const
+//	{
+//		return m_width;
+//	}
+//
+//	std::int32_t height() const
+//	{
+//		return m_height;
+//	}
+//
+//	bool saveBMP(const std::string& path)
+//	{
+//		const std::int32_t  rowSize = m_width * 3 + m_width % 4;
+//		const std::uint32_t bmpsize = rowSize * m_height;
+//		const BMPHeader header =
+//		{
+//			0x4d42,
+//			static_cast<std::uint32_t>(bmpsize + sizeof(BMPHeader)),
+//			0,
+//			0,
+//			sizeof(BMPHeader),
+//			40,
+//			m_width,
+//			m_height,
+//			1,
+//			24,
+//			0,
+//			bmpsize,
+//			0,
+//			0,
+//			0,
+//			0
+//		};
+//
+//		if (std::ofstream ofs{ path, std::ios_base::binary })
+//		{
+//			ofs.write(reinterpret_cast<const char*>(&header), sizeof(header));
+//
+//			std::vector<std::uint8_t> line(rowSize);
+//
+//			for (std::int32_t y = m_height - 1; -1 < y; --y)
+//			{
+//				size_t pos = 0;
+//
+//				for (std::int32_t x = 0; x < m_width; ++x)
+//				{
+//					const RGB& col = m_data[static_cast<std::size_t>(y) * m_width + x];
+//					line[pos++] = ToUint8(col.b);
+//					line[pos++] = ToUint8(col.g);
+//					line[pos++] = ToUint8(col.r);
+//				}
+//
+//				ofs.write(reinterpret_cast<const char*>(line.data()), line.size());
+//			}
+//
+//			return true;
+//		}
+//		else
+//		{
+//			return false;
+//		}
+//	}
+//};
+//
+//int main()
+//{
+//
+//	Image image(512, 512);
+//
+//	std::cout << "---------------------------------\n";
+//	std::cout << "* frequency [0.1 .. 8.0 .. 64.0] \n";
+//	std::cout << "* octaves   [1 .. 8 .. 16]       \n";
+//	std::cout << "* seed      [0 .. 2^32-1]        \n";
+//	std::cout << "---------------------------------\n";
+//
+//	while()
+//	{
+//		double frequency;
+//		std::cout << "double frequency = ";
+//		std::cin >> frequency;
+//		frequency = std::clamp(frequency, 0.1, 64.0);
+//
+//		std::int32_t octaves;
+//		std::cout << "int32 octaves    = ";
+//		std::cin >> octaves;
+//		octaves = std::clamp(octaves, 1, 16);
+//
+//		std::uint32_t seed;
+//		std::cout << "uint32 seed      = ";
+//		std::cin >> seed;
+//
+//		const siv::PerlinNoise perlin(seed);
+//		const double fx = image.width() / frequency;
+//		const double fy = image.height() / frequency;
+//
+//		for (std::int32_t y = 0; y < image.height(); ++y)
+//		{
+//			for (std::int32_t x = 0; x < image.width(); ++x)
+//			{
+//				const RGB color(perlin.accumulatedOctaveNoise2D_0_1(x / fx, y / fy, octaves));
+//				image.set(x, y, color);
+//			}
+//		}
+//
+//		std::stringstream ss;
+//		ss << 'f' << frequency << 'o' << octaves << '_' << seed << ".bmp";
+//
+//		if (image.saveBMP(ss.str()))
+//		{
+//			std::cout << "...saved \"" << ss.str() << "\"\n";
+//		}
+//		else
+//		{
+//			std::cout << "...failed\n";
+//		}
+//		
+//		char c;
+//		std::cout << "continue? [y/n] >";
+//		std::cin >> c;
+//		if (c != 'y') break;
+//		std::cout << '\n';
+//	}
+//}
+
